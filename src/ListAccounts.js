101ddcf77f56aca540ee497b6760d08b8bea0d66
Biniam Asnake <biniamasnake@gmail.com>
2023-04-12 15:40:50 +0200

    - Refactored API url to work on both local and production

--- src/ListAccounts.js
+++ src/ListAccounts.js
@@ -23,7 +23,7 @@ function ListAccounts() {
         {field: 'dateOfBirth', headerName: 'Date of Birth', width: 130},
         {field: 'phoneNumber', headerName: 'Phone Number', width: 130},
         {field: 'email', headerName: 'Email', width: 200},
-        {field: 'pin', headerName: 'Pin', width: 130}, //, valueGetter: (params) => `${'*'.repeat(params.row.pin.length)}`},
+        {field: 'pin', headerName: 'Pin', width: 130}, //, valueGetter: (params) => `${'*'.repeat(params.row.pin..toString().length)}`},
         {field: 'balance', headerName: 'Balance', width: 130, sortable: false,},
         {field: 'openingDate', headerName: 'Opening Date', width: 130},
     ];
